ad1c02ab8ae19ceb01ab4518780909992ad20f57
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-06-08 11:08:54 +0530

    Update calculateDiscount.js

--- ezpark-backend/Server/api_operations_1/operations_1/use_p_system/calculateDiscount.js
+++ ezpark-backend/Server/api_operations_1/operations_1/use_p_system/calculateDiscount.js
@@ -1,10 +1,12 @@
-// need to calculate discount rate(using discount_details table) for the badge_level and discount amount for a payment 
-
 const connection = require('../../../service/connection');
 const queries = require("../../../sql/sql");
 
+// Function to calculate the discount rate for a badge level and discount amount for a payment
 async function calculateDiscount(req, res) {
+  // Extract badge ID and badge name from the request body
   const { badge_id, badge_name } = req.body;
+
+  // Retrieve discount details from the database based on the badge ID
   connection.query(
     queries.get_discount_by_badge_id,
     [badge_id],
@@ -12,21 +14,27 @@ async function calculateDiscount(req, res) {
       if (err) throw err;
       const discount_data = data[0];
       const discount = { discount_name: null, discount_precentage: 0 };
+
+      // Check if the discount has an expiration date
       if (discount_data.ExpDate != null) {
         if (discount_data.ExpDate < new Date()) {
+          // Use default discount if expired
           discount.discount_precentage = discount_data.DefaultDiscount;
           discount.discount_name = badge_name;
         } else {
+          // Use specific discount if not expired
           discount.discount_precentage = discount_data.Discount;
           discount.discount_name = discount_data.Discounts_Name;
         }
       } else {
+        // Use default discount if no expiration date
         discount.discount_precentage = discount_data.DefaultDiscount;
         discount.discount_name = badge_name;
       }
-      res.status(200).send(discount);
+
+      res.status(200).send(discount); // Send the calculated discount to the client
     }
   );
-}
+};
 
-module.exports = {calculateDiscount}
+module.exports = { calculateDiscount };
