9f8ec840be2a4c701f561a650238ad424bf57fff
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-06-24 01:14:25 +0530

    some changes

--- ezpark-web/frontend/src/Components/PaymentCustom/PaymentScreen.jsx
+++ ezpark-web/frontend/src/Components/PaymentCustom/PaymentScreen.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
-import CheckoutForm from "./CheckoutForm";
 import "./PaymentScreen.css";
+import CheckoutForm from "./CheckoutForm";
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
 const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+
 
 export default function PaymentScreen() {
   const [clientSecret, setClientSecret] = useState("");
